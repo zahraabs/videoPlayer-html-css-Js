8254a091567d61e0172dffeab350914c81d9a3d0
zahraabs <zahraabbasiun@gmail.com>
2023-08-05 02:07:55 +0330

    some functions for time and rewind added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 let player = document.querySelector(".myplayer");
 let video = player.querySelector("#myVideo");
 let controls = player.querySelector(".myplayer__controls");
-let play = controls.querySelector(".play");
 
-play.addEventListener("click" , playOrPause);
+let play = controls.querySelector(".play");
+let rewind = controls.querySelector(".rewind");
+let forward = controls.querySelector(".forward");
+let currentTime = controls.querySelector(".currentTime");
+let videoTime = controls.querySelector(".videoTime");
+
+play.addEventListener("click", playOrPause);
+rewind.addEventListener("click", back5Sec);
+forward.addEventListener("click", go5Sec);
+
+video.addEventListener("timeupdate", updateVideoDetails);
 
 function playOrPause() {
-   if (video.paused) {
-   video.play();
-   } else {
-    video.pause();
-   }
+    videoTime.textContent = getTime(video.duration)
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 
-   togglePlayIcon();
+    togglePlayIcon();
 }
 
 function togglePlayIcon() {
@@ -20,4 +30,22 @@ function togglePlayIcon() {
 
     icon.classList.toggle("ion-play");
     icon.classList.toggle("ion-pause");
+}
+
+function back5Sec() {
+    video.currentTime -= 5;
+}
+function go5Sec() {
+    video.currentTime += 5;
+}
+
+function updateVideoDetails() {
+    currentTime.textContent = getTime(video.currentTime)
+}
+
+function getTime(time) {
+    let min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    let formatedTime = (min < 10 ? "0" : "") + min + ":" + (sec < 10 ? "0" : "") + sec;
+    return formatedTime;
 }
